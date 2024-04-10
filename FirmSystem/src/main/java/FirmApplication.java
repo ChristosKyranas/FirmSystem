5784c96bb34587e5a538e408af08f123282790b9
christos <christos_k_@hotmail.com>
2024-04-10 14:22:42 +0300

    update application with login form-page

--- FirmSystem/src/main/java/FirmApplication.java
+++ FirmSystem/src/main/java/FirmApplication.java
@@ -1,16 +1,43 @@
+import GUI.Dialogs.LoginForm;
 import GUI.FirmGuiApplication;
+import domain.User;
 import service.FirmServiceImpl;
 
 import javax.swing.*;
 
 public class FirmApplication {
 
+//    public FirmApplication(){
+//        JFrame frame = new JFrame("FirmApplication");
+//        frame.setContentPane(new FirmGuiApplication().getPanel());
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setVisible(true);
+//
+//    }
     public static void main(String[] args) {
-        JFrame frame = new JFrame("FirmApplication");
-        frame.setContentPane(new FirmGuiApplication().getPanel());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+
+        LoginForm loginForm = new LoginForm(null);
+        User user = loginForm.user;
+
+        if(user!=null){
+            System.out.println("Successful Authentication of " + user.name);
+            System.out.println("\t Email " + user.email);
+            System.out.println("\t Phone " + user.phone);
+            System.out.println("\t Address " + user.address);
+            JFrame frame = new JFrame("FirmApplication");
+            frame.setContentPane(new FirmGuiApplication().getPanel());
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        } else {
+            System.out.println("Authentication Canceled");
+        }
+//        JFrame frame = new JFrame("FirmApplication");
+//        frame.setContentPane(new FirmGuiApplication().getPanel());
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setVisible(true);
 
     }
 }
