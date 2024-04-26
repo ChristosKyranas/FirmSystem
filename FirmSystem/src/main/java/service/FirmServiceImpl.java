289be30c3c885762148764e31c701665e5c59b4d
christos <christos_k_@hotmail.com>
2024-04-26 15:33:17 +0300

    Delete or Active option

--- FirmSystem/src/main/java/service/FirmServiceImpl.java
+++ FirmSystem/src/main/java/service/FirmServiceImpl.java
@@ -8,9 +8,16 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static enumaration.MessageEnum.DUPLICATE_ENTRY_FIRM_NAME;
+import static enumaration.MessageEnum.INVALID_EMPTY_FIRM_NAME;
+
 public class FirmServiceImpl implements FirmService {
 
+    // 0 -> delete or 1 -> active
+    private static final int DELETE_OR_ACTIVE = 0;
+
     private static final String GET_ALL_FIRMS = "SELECT * FROM company.firm ORDER BY name ASC";
+    private static final String GET_ALL_ACTIVE_FIRMS = "SELECT * FROM company.firm WHERE active = 1 ORDER BY name ASC";
 //    private static final String FIRM_ID = "firm_id";
     private static final String NAME = "name";
 
@@ -24,6 +31,7 @@ public class FirmServiceImpl implements FirmService {
 
     /**
      * Get all firms from database
+     *
      * @return List<String>
      */
     public List<String> getAllFirms(){
@@ -31,7 +39,9 @@ public class FirmServiceImpl implements FirmService {
         List<String> orderedListFirm = new ArrayList<>();
         try {
             Statement statement = makeConnection().createStatement();
-            ResultSet rs = statement.executeQuery(GET_ALL_FIRMS);
+            ResultSet rs;
+            rs = (DELETE_OR_ACTIVE == 0) ?  statement.executeQuery(GET_ALL_FIRMS) : statement.executeQuery(GET_ALL_ACTIVE_FIRMS);
+
             while(rs.next()){
                 //firmHashMap.put(Integer.valueOf(rs.getString(FIRM_ID)), rs.getString(NAME));
                 //System.out.println("Firms: " + firmHashMap.toString());
@@ -71,6 +81,9 @@ public class FirmServiceImpl implements FirmService {
             makeConnection().close();
         } catch (SQLIntegrityConstraintViolationException e) {
             System.out.println("Duplicate entry: " + firmName + " already exists");
+
+            System.out.println(DUPLICATE_ENTRY_FIRM_NAME.getMessage() + firmName);
+
             JOptionPane.showMessageDialog(FirmForm,
                     "Firm already exists.\nEnter a different firm name.",
                     "Try Again",
@@ -81,12 +94,18 @@ public class FirmServiceImpl implements FirmService {
         }
 	}
 
+    /**
+     * delete firm from
+     * @param firmName
+     */
     public void removeFirm(String firmName){
         try {
             Statement statement = makeConnection().createStatement();
-            String query = "delete " +
-                    "from company.firm " +
-                    "where name = '"+ firmName +"'";
+//            String query = "DELETE FROM company.firm WHERE name = '"+ firmName +"'";
+
+            String query;
+            query = (DELETE_OR_ACTIVE == 0 ) ? "DELETE FROM company.firm WHERE name = '"+ firmName +"'" : "UPDATE company.firm SET active = 0 WHERE name = '"+ firmName +"'";
+
             System.out.println(query);
             statement.executeUpdate(query);
 
@@ -98,7 +117,7 @@ public class FirmServiceImpl implements FirmService {
         }
     }
 
-    public List<String> findSelectedFirm(String selectedFirm){
+    public List<String> getFirm(String selectedFirm){
         List<String> firm = new ArrayList<>();
         try {
             Statement statement = makeConnection().createStatement();
