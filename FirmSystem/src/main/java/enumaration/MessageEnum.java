40a6a50ed8bceab6762d68a3f17120da617ad66b
christos <christos_k_@hotmail.com>
2024-04-26 15:32:33 +0300

    Update messages

--- FirmSystem/src/main/java/enumaration/MessageEnum.java
+++ FirmSystem/src/main/java/enumaration/MessageEnum.java
@@ -2,7 +2,9 @@ package enumaration;
 
 public enum MessageEnum {
 
-	INVALID_EMPTY_FIRM_NAME("Firm name cannot be empty");
+	INVALID_EMPTY_FIRM_NAME("Firm name cannot be empty"),
+	DUPLICATE_ENTRY_FIRM_NAME("Duplicate Firm name: "),
+	INVALID_ALPHANUMERICAL_FIRM_NAME("Firm name must be Alphanumeric or &");
 
 	private final String message;
 
