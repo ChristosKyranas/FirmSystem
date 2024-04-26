113d789a7ff926d95d7939ad9eb0f3c4aa587b55
christos <christos_k_@hotmail.com>
2024-04-26 15:35:16 +0300

    Fix comments and messages

--- FirmSystem/src/main/java/service/BranchServiceImpl.java
+++ FirmSystem/src/main/java/service/BranchServiceImpl.java
@@ -1,17 +1,22 @@
 package service;
 
 import GUI.Dialogs.CreateMessageGui;
+import GUI.Dialogs.LoginForm;
 import domain.Branch;
 import factory.DatabaseConnectionFactory;
+import utils.ANSI;
 
 import javax.swing.*;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static utils.ANSI.*;
+
 public class BranchServiceImpl implements BranchService {
 
     private static final String GET_ALL_BRANCHES = "SELECT * FROM company.branch ORDER BY name ASC";
+    private static final String GET_ALL_ACTIVE_BRANCHES = "SELECT * FROM company.branch WHERE active = 1 ORDER BY name ASC";
     private static final String BRANCH_ID = "branch_id";
     private static final String NAME = "name";
     private static final String ADDRESS = "address";
@@ -38,7 +43,7 @@ public class BranchServiceImpl implements BranchService {
         List<Branch> orderedListBranch = new ArrayList<>();
         try {
             Statement statement = makeConnection().createStatement();
-            ResultSet rs = statement.executeQuery(GET_ALL_BRANCHES);
+            ResultSet rs = statement.executeQuery(GET_ALL_ACTIVE_BRANCHES);
 
             while(rs.next()){
              //   branchHashMap.put(Integer.valueOf(rs.getString(BRANCH_ID)), getBranch());
@@ -57,7 +62,8 @@ public class BranchServiceImpl implements BranchService {
     }
 
     /**
-     * Add new Branch in Database - Check if exists
+     * Add new Branch in Database
+     *
      * @param branch
      */
     public void addBranch(Branch branch){
@@ -67,33 +73,40 @@ public class BranchServiceImpl implements BranchService {
 //            if(branch.getName()!=null && branch.getAddress()!=null && branch.getCity()!=null && branch.getCountry()!=null && branch.getEstablishment()!=null ){
             System.out.println("test");
             System.out.println(branch.getName());
-            String query = "SELECT * FROM company.branch WHERE name=?";
-            PreparedStatement preparedStatement = makeConnection().prepareStatement(query);
-            preparedStatement.setString(1, branch.getName());
+
+            // check for duplicate
+            String query = "SELECT count(name) FROM company.branch WHERE name='" + branch.getName() + "'";
+//            String query = "SELECT * FROM company.branch WHERE name=?";
+//            PreparedStatement preparedStatement = makeConnection().prepareStatement(query);
+//            preparedStatement.setString(1, branch.getName());
 
             System.out.println(query);
             ResultSet rs = statement.executeQuery(query);
-//                    "select * " +
-//                    "from company.branch" +
-////                    "where name = '"+ branch.getName()+
-//                    "where name = 't' ");
-//                    "' and address = '"+ branch.getAddress()+
-//                    "' and city = '"+ branch.getCity()+
-//                    "' and country = '"+ branch.getCountry()+
-//                        "' and establishment = '"+ branch.getEstablishment()+
-            System.out.println(rs.next());
-//            if(!(isNullOrEmpty(branch.getName()) || isNullOrEmpty(branch.getAddress()) || isNullOrEmpty(branch.getCity()) || isNullOrEmpty(branch.getCountry()) || isNullOrEmpty(branch.getEstablishment())) ){
-//
-//                if (rs==null){
-//                    System.out.println("test2");
-//                }
-//            }
-//            query = "insert into company.firm (name) values ('" + firmName + "');";
 
-            // if not exists insert
-            if(!rs.next()){
-                query = "INSERT INTO company.branch ( name, address, city, country, establishment, budget, worth, firm ) " +
-                        "VALUES ('"+ branch.getName()+"','"+ branch.getAddress()+"','"+ branch.getCity()+"','"+ branch.getCountry()+"','"+ branch.getEstablishment()+"','"+ branch.getBudget()+"','"+ branch.getWorth()+"',"+ branch.getFirm()+");";
+            System.out.println(ANSI_CYAN + rs.next() + ANSI_RESET);
+
+            // check if firm_id exists
+            Statement statement2 = makeConnection().createStatement();
+//            String query2 = "SELECT count(firm.name) FROM firm INNER JOIN branch ON firm_id = ?";
+            String query2 = "SELECT count(firm.name) FROM firm INNER JOIN branch ON firm_id = " + branch.getFirm() +"";
+            System.out.println(query2);
+            ResultSet rs2 = statement2.executeQuery(query2);
+
+            System.out.println(ANSI_BLUE + rs2.next() + ANSI_RESET);
+
+            // show message that there is no firm with this firm_id
+            if(!rs2.next()){
+//                JOptionPane.showMessageDialog(LoginForm.this,
+//                        "Email or Password Invalid",
+//                        "Try Again",
+//                        JOptionPane.ERROR_MESSAGE);
+                System.out.println("Firm_id does not exist");
+            }
+
+            // if doesn t exist && firm_id is valid then insert branch
+            if(!rs.next() && rs2.next()){
+                query = "INSERT INTO company.branch ( name, address, city, country, establishment, budget, worth, firm, active ) " +
+                        "VALUES ('"+ branch.getName()+"','"+ branch.getAddress()+"','"+ branch.getCity()+"','"+ branch.getCountry()+"','"+ branch.getEstablishment()+"','"+ branch.getBudget()+"','"+ branch.getWorth()+"',"+ branch.getFirm()+",1);";
                 System.out.println(query);
                 statement.executeUpdate(query);
             }
@@ -106,13 +119,13 @@ public class BranchServiceImpl implements BranchService {
             //if it exists -> reject it
             //query = null
             //otherwise -> add it
-            new CreateMessageGui(branchJScrollPane, "test");
+//            new CreateMessageGui(branchJScrollPane, "test");
         }catch (SQLException e){
-            new CreateMessageGui(branchJScrollPane, "test");
+//            new CreateMessageGui(branchJScrollPane, "test");
             System.out.println("Error Code: " + e.getErrorCode());
             System.out.println("SQL State: " + e.getSQLState());
             System.out.println("Message: " + e.getMessage());
-            System.out.println("Message: " + e.getMessage());
+
             System.out.println("Print Trace: /n");
             e.printStackTrace();
         }
@@ -171,14 +184,18 @@ public class BranchServiceImpl implements BranchService {
         return null;
     }
 
+    /**
+     * set branch inactive - "delete"
+     *
+     * @param selectedBranch
+     * @param country
+     * @param city
+     */
     public void removeBranch(String selectedBranch, String country, String city){
         try {
             Statement statement = makeConnection().createStatement();
-            String query =  "delete  " +
-                     "from company.branch " +
-                     "where name = '"+ selectedBranch + "' " +
-                            "and country = '" + country +"' " +
-                            "and city = '" + city + "'" ;
+//            String query =  "DELETE FROM company.branch WHERE name = '"+ selectedBranch + "' AND country = '" + country + "' AND city = '" + city + "'" ;
+            String query =  "UPDATE company.branch SET active = 0 WHERE name = '"+ selectedBranch + "' AND country = '" + country + "' AND city = '" + city + "'" ;
             System.out.println(query);
             statement.executeUpdate(query);
 
@@ -190,6 +207,7 @@ public class BranchServiceImpl implements BranchService {
             System.out.println("SQL State: " + e.getSQLState());
             System.out.println("Message: " + e.getMessage());
             System.out.println("Message: " + e.getMessage());
+
             System.out.println("Print Trace: /n");
             e.printStackTrace();
         }
